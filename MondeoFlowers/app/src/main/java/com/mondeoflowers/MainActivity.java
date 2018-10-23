6c4d5d8efff807d319a235bd59c3eb70247eab65
Semih Erdogdu <semih.erdogdu@student.pxl.be>
2018-10-23 13:22:48 +0200

    created layout

--- MondeoFlowers/app/src/main/java/com/mondeoflowers/MainActivity.java
+++ MondeoFlowers/app/src/main/java/com/mondeoflowers/MainActivity.java
@@ -2,12 +2,27 @@ package com.mondeoflowers;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnExample;
+    TextView textExample;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnExample = (Button) findViewById(R.id.startButton);
+        textExample = (TextView) findViewById(R.id.mainTextView);
+        btnExample.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                textExample.setText("Button clicked");
+            }
+        });
     }
 }
